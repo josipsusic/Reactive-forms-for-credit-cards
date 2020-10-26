2254c7e450272c1ae59db75502270956c00f26be
josipsusic <susic@uhp-digital.com>
2020-10-26 10:28:47 +0100

    Proof of concept

--- src/app/classes/date-form-control.ts
+++ src/app/classes/date-form-control.ts
@@ -3,6 +3,25 @@ import { FormControl } from '@angular/forms';
 export class DateFormControl extends FormControl {
   //Override setValue method to run some custom code on input
   setValue(value: string, options: any) {
+    // Disable everything except numbers or a forward slash
+    if (value.match(/[^0-9|\/]/gi)) {
+      // Then revert to the current value
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      // And don't run anything else
+      return;
+    }
+
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2 && this.value.length === 3) {
+      // Let user's value pass through
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
     if (value.length === 2) {
       super.setValue(value + '/', { ...options, emitModelToViewChange: true });
       return;
